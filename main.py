964b9b914b33ec30b710d1fb7513719b778e8383
579 <simondumesny@gmail.com>
2021-04-25 13:50:08 +0200

    removed all useless print()

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import time
 from pypresence import Presence
 with open("app.json", "r") as read:
     file=json.load(read)
-print(file)
 RPC = Presence(835141269801009176)
 a=0
 actual=0
@@ -14,27 +13,20 @@ close=True
 while True:
 	pids=psutil.pids()
 	for i in range(0,len(pids)) :
-		print('ca marche la')
 		for x,var in file["app"].items():
 			try:
 				process= psutil.Process(pids[i])
 			except:
-				print(4)
-				#i=0
-			print(2)
+				pass
 			try:
 				if process.name()==x and process.status()=='running':
-					print(3)
 					actual=process
-					print(4)
 					RPC=Presence(file['app'][x]['id'])
 					RPC.connect()
 					while close is True:
 						try:
 							process.status()
-							print(5)
 							RPC.update(state=str(file['app'][x]['state']),large_image="icon",start=start_time,)
-							#a=1
 						except:
 							close=False
 					RPC.clear()
