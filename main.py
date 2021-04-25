84a0f72e223a986e20aa52dde76cc35be0acc8ea
579 <simondumesny@gmail.com>
2021-04-25 12:37:29 +0200

    things now work correctly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import psutil
 import json
+from time import*
 import time
 from pypresence import Presence
 with open("app.json", "r") as read:
@@ -9,30 +10,45 @@ RPC = Presence(835141269801009176)
 a=0
 actual=0
 start_time=time.time()
+close=True
 while True:
 	pids=psutil.pids()
 	for i in range(0,len(pids)) :
+		print('ca marche la')
 		for x,var in file["app"].items():
 			try:
 				process= psutil.Process(pids[i])
-				if process.name()==x and process.status()=='running':
-					actual=process
-					RPC = Presence(file['app'][x]['id'])
-					RPC.connect() 
-					RPC.update(state=str(file['app'][x]['state']),
-						large_image="icon",start=start_time,)
-					a=1
-					print(process.status())
-				elif process.name()==x:
-					print(closing)
 			except:
-				i=0
-				#sleep(1)
-	try:
+				print(4)
+				#i=0
+			print(2)
+			try:
+				if process.name()==x and process.status()=='running':
+					print(3)
+					actual=process
+					print(4)
+					RPC=Presence(file['app'][x]['id'])
+					RPC.connect()
+					while close is True:
+						try:
+							process.status()
+							print(5)
+							RPC.update(state=str(file['app'][x]['state']),large_image="icon",start=start_time,)
+							#a=1
+						except:
+							close=False
+					RPC.clear()
+			except:
+				pass
+		close=True
+		x=0
+		i=0
+	sleep(1)
+	'''try:
 		if a==1 and actual.status()=='sleeping':
 			print('sleeping')
 		else:
-			RPC.clear()
+			#RPC.clear()
 			print("cleaned!")
 	except:
-		pass
+		pass'''
